0a8e2c87e251ff75d6fee742dee2fb5725222c11
Sahra <sasta8195@uib.no>
2026-08-17 20:20:57 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/sahra/strava_art/StravaArtApplication.java
@@ -0,0 +1,13 @@
+package com.sahra.strava_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahra/strava_art/StravaArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahra.strava_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
